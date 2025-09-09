165173095e6574b75656020cb739aa0f973995f1
ngthanhs1 <thanhtuyen191205@gmail.com>
2025-09-09 10:54:02 +0000

    up

--- Hoctap/routes/web.php
+++ Hoctap/routes/web.php
@@ -4,7 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-
-
 });
 Route::get('/Thuvien','App\Http\Controllers\ThuvienController@displayThuvien');
